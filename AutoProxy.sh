098275849633a84a22cceb17b262ddba56d7fc6a
alekseyvlasov1991 <alekseyvlasov1991@yandex.ru>
2024-07-07 16:53:41 +0300

    Update AutoProxy.sh

--- AutoProxy.sh
+++ AutoProxy.sh
@@ -828,7 +828,7 @@ echo "net.ipv4.tcp_rmem=4096 87380 6291456" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_wmem=4096 16384 6291456" >> /etc/sysctl.conf
 sysctl -p
 
-#delete_file_if_exists $script_log_file;
+delete_file_if_exists $script_log_file;
 check_startup_parameters;
 check_ipv6;
 if is_proxyserver_installed; then
@@ -848,21 +848,21 @@ array=( 1 2 3 4 5 6 7 8 9 0 a b c d e f )
 function rh () { echo ${array[$RANDOM%16]}; }
 
 rnd_subnet_ip () {
-  echo -n $(get_subnet_mask)
+  echo -n $(get_subnet_mask);
   symbol=$subnet
   while (( $symbol < 128)); do
     if (($symbol % 16 == 0)); then echo -n :; fi;
     echo -n $(rh);
     let "symbol += 4";
   done;
-  echo ;
+  echo;
 }
 
 # Temporary variable to count generated ip's in cycle
 count=1
 
 # Generate random 'proxy_count' ipv6 of specified subnet and write it to 'ip.list' file
-while [ "$count" -le 2000 ]
+while [ "\$count" -le $proxy_count ]
 do
   rnd_subnet_ip >> $random_ipv6_list_file;
   ((count+=1))
